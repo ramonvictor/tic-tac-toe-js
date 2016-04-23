18e11957bbe9ba10b4811c2db4959d0031fa57d5
Ramon Bezerra <ramon.bezerra@booking.com>
2016-04-23 23:13:01 +0200

    add env port variable check

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -15,8 +16,4 @@ io.on('connection', function(socket) {
 	});
 });
 
-var port = process.env.PORT || 3000;
-
-http.listen(3000, function() {
-  // console.log('listening on *:3000');
-});
+http.listen(port);
